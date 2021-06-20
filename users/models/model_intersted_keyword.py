a9af79d6e15282cf2c4f570dd48e418687d58906
min.ming_ <74173368+z132305@users.noreply.github.com>
2021-06-20 12:22:00 +0900

    Modify: users/models/model_interested_keyword worry Foreignkey 연결

--- users/models/model_intersted_keyword.py
+++ users/models/model_intersted_keyword.py
@@ -2,7 +2,8 @@ from users.models.model_worry import Worry
 from django.db import models
 
 class InterestedKeyword(models.Model):
-    name = models.CharField(max_length = 45)
+    name  = models.CharField(max_length = 45)
+    worry = models.ForeignKey('users.Worry', on_delete = models.SET_NULL, null = True)
 
     class Meta:
         db_table = 'interested_keywords'
@@ -10,7 +11,6 @@ class InterestedKeyword(models.Model):
 class UserInterestedKeyword(models.Model):
     general_user       = models.ForeignKey('users.GeneralUser', on_delete = models.CASCADE)
     interested_keyword = models.ForeignKey('users.InterestedKeyword', on_delete = models.SET_NULL, null = True)
-    worry              = models.ForeignKey('users.Worry', on_delete = models.SET_NULL, null = True)
 
     class Meta:
         db_table = 'user_interested_keywords'
